83fec974ea9dfbfee75964f4ceb120dc0da7b399
trchagas <49379007+trchagas@users.noreply.github.com>
2020-05-23 23:44:58 -0300

    Add files via upload

--- api/controllers/calendar.controller.js
+++ api/controllers/calendar.controller.js
@@ -7,12 +7,22 @@ exports.getLook = async (req, res) => {
         // Esse rota deve retornar o look usado pelo usuário no dia (parâmetro day)
         // e turno (parâmetro shift) especificado.
 
-        // Se não houver look neste dia, retorne null.
-
         // Preste atenção para padronizar o dia recebido de forma que a hora não influencie
         // o resultado da busca. Use apenas o dia (e.g. 31/12/2000).
+        const look = await Look.findOne(
+            {
+                "day":   req.params.day.getDate(),
+                "shift": req.params.shift
+            }
+        );
+
+        // Se não houver look neste dia, retorne null.
+        if(!look){
+            return null;
+        }
         
         // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
+        return res.json(look);
     }
     catch(err) {
         console.error(err, err.message, err.stack);
@@ -31,12 +41,36 @@ exports.setLook = async (req, res) => {
         // Preste atenção para garantir que o look recebido realmente pertence ao usuário
         // que está fazendo a request.
 
-        // Se já houver um look atribuído para esse mesmo dia e turno, sobrescreva-o.
-
         // Preste atenção para padronizar o dia recebido de forma que a hora não influencie
         // ou não fique salva no banco. Use apenas o dia (e.g. 31/12/2000).
+        let look = await Look.findOne(
+            {
+                "day":   req.user.params.day.getDate(),
+                "shift": req.user.params.shift
+            }
+        );
+
+        // Se já houver um look atribuído para esse mesmo dia e turno, sobrescreva-o.
+        if(!look){
+            look = await Look.create(
+                {
+                    "id":    req.user.params.id,
+                    "day":   req.user.params.day.getDate(),
+                    "shift": req.user.params.shift
+                }
+            );
+        } else {
+            look = await Look.update(
+                {
+                    "id":    req.user.params.id,
+                    "day":   req.user.params.day.getDate(),
+                    "shift": req.user.params.shift
+                }
+            );
+        }
         
         // Pesquise qual deve ser o código de retorno HTTP quando a requisição foi bem sucedida.
+        return res.json(look);
     }
     catch(err) {
         console.error(err, err.message, err.stack);
